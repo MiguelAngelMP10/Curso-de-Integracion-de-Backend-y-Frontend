cdc266c669be9c58a9beb986d6d4ac0309ccbdca
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-05-26 17:55:29 -0500

    Configuración de fileMocks y styleMocks

--- src/frontend/__test__/setupTest.js
+++ src/frontend/__test__/setupTest.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { configure } from "enzyme";
 
+// eslint-disable-next-line import/no-unresolved
 import Adapter from "enzyne-adapter-react-16";
 
 configure({ adapter: new Adapter() });
