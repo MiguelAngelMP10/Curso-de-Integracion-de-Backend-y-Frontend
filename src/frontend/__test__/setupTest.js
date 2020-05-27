ea0c10b1e6531c7ec180eafea81c7964fb8ce0d9
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-05-27 08:52:30 -0500

    Implementando snapshots

--- src/frontend/__test__/setupTest.js
+++ src/frontend/__test__/setupTest.js
@@ -1,7 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { configure } from "enzyme";
-
-// eslint-disable-next-line import/no-unresolved
-import Adapter from "enzyne-adapter-react-16";
+import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
