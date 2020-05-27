da7468ae262df41640ed1e6eac6e834ffbb5800f
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-05-27 10:46:06 -0500

    Deploy Platzi Video Intento

--- src/server/server.js
+++ src/server/server.js
@@ -43,7 +43,7 @@ if (ENV === "development") {
   app.use(webpackHotMiddleware(compiler));
 } else {
   app.use((req, res, next) => {
-    if (!req.hashManifest) res.hashManifest = getManifest();
+    if (!req.hashManifest) req.hashManifest = getManifest();
     next();
   });
   app.use(helmet());
